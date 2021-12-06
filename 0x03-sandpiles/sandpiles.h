9e96d76fdf7a831f4017f0ebda1e05442970732d
szbrooks2017 <strattonbrooks13@gmail.com>
2021-12-06 03:15:43 +0000

    adding header

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -8,7 +8,7 @@
 /* prototypes */
 static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int is_stable(int *grid);
-void topple(int *grid);
+void topple(int grid1[3][3], int grid2[3][3]);
+int add_sandpile_matrix(int grid1[3][3], int grid2[3][3]);
 
 #endif
