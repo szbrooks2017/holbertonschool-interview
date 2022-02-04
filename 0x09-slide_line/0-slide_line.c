08ceba11142f8af82749c0fa2d9cc23ec50c9ba1
szbrooks2017 <strattonbrooks13@gmail.com>
2022-02-04 02:45:40 +0000

    correct output finally

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -30,7 +30,46 @@ int slide_line(int *line, size_t size, int direction)
  */
 void slide_left(int *line, size_t size)
 {
+	int i, position = 0, n = 0;
 
+	for (i = 0; i < (int)size; i++)
+	{
+		if (line[i] == 0)
+			position++;
+
+		if (line[i] > 0)
+			n++;
+
+		if (line[i] > 0 && position > 0)
+		{
+			line[i - position] = line[i];
+			line[i] = 0;
+			i = i - position;
+			position = 0;
+		}
+	}
+	position = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1 && line[i] == line[i + 1])
+		{
+			line[position] = line[i] + line[i + 1];
+			line[i + 1] = 0;
+			if (position != i)
+				line[i] = 0;
+			position++;
+		}
+		else if (line[i] > 0 && position > 0)
+		{
+			line[position] = line[i];
+			if (position != i)
+				line[i] = 0;
+			position++;
+		}
+		else if (line[i] > 0)
+			position++;
+	}
 }
 
 /**
@@ -40,5 +79,44 @@ void slide_left(int *line, size_t size)
  */
 void slide_right(int *line, size_t size)
 {
-	
+	int i, position = 0, max = size - 1, n = 0;
+
+	for (i = max; i >= 0; i--)
+	{
+		if (line[i] == 0)
+			position++;
+
+		if (line[i] > 0)
+			n++;
+
+		if (line[i] > 0 && position > 0)
+		{
+			line[i + position] = line[i];
+			line[i] = 0;
+			i = i + position;
+			position = 0;
+		}
+	}
+	position = max;
+
+	for (i = max; i > max - n; i--)
+	{
+		if (i > max - n + 1 && line[i] == line[i - 1])
+		{
+			line[position] = line[i] + line[i - 1];
+			line[i - 1] = 0;
+			if (position != i)
+				line[i] = 0;
+			position--;
+		}
+		else if (line[i] > 0 && position < max)
+		{
+			line[position] = line[i];
+			if (position != i)
+				line[i] = 0;
+			position--;
+		}
+		else if (line[i] > 0)
+			position--;
+	}
 }
