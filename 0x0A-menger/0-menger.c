7f630a63bdc00ebfc6bf3e35d7dae6e69dd1b37e
szbrooks2017 <strattonbrooks13@gmail.com>
2022-02-17 23:57:33 +0000

    adding basic math

---
+++ 0x0A-menger/0-menger.c
@@ -0,0 +1,40 @@
+#include "menger.h"
+#include <math.h>
+
+/**
+ * menger - draws a 2d menger sponge
+ * 
+ * @level: is the level of the menger sponge to draw 
+ */
+
+void menger(int level)
+{
+	int i, j, size, dh, dw;
+
+	size = pow(3, level);
+ 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			dh = i, dw = j;
+			while (1)
+			{
+				if (dh % 3 == 1 && dw % 3 == 1)
+				{
+					printf(" ");
+					break;
+				}
+				if (dh == 0 || dw == 0)
+				{
+					printf("#");
+					break;
+				}
+				dh /= 3;
+				dw /= 3;
+
+			}
+		}
+		printf("\n");
+	}
+}
---
+++ 0x0A-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENG_H
+#define MENG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPE */
+void menger(int level);
+
+#endif /* MENG_H */
