ad026021ad8ac72190411d6b58c856774e7b0447
szbrooks2017 <strattonbrooks13@gmail.com>
2022-03-08 19:47:09 +0000

    solved

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -4,14 +4,17 @@ def rain(walls):
     water = 0
     if len(walls) == 0:
         return 0
-    middle = int(len(walls) / 2)
-    for i in range(len(walls)):
-        if i != 0 and i != (len(walls) - 1):
-            l = max(walls[0:middle])
-            r = max(walls[middle:len(walls)])
-            least = min(l, r)
-            if least - walls[i] >= 0:
-                water += least - walls[i]
-            else:
-                water += 0
+    left, right = 0, len(walls) - 1
+    leftMax, rightMax = walls[left], walls[right]
+    water = 0
+
+    while left < right:
+        if leftMax < rightMax:
+            left += 1
+            leftMax = max(leftMax, walls[left])
+            water += leftMax - walls[left]
+        else:
+            right -= 1
+            rightMax = max(rightMax, walls[right])
+            water += rightMax - walls[right]
     return water
