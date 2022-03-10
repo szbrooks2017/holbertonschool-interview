77f17169f544a33f4beb5db8a00530f4f9f0c877
szbrooks2017 <strattonbrooks13@gmail.com>
2022-03-10 18:54:33 +0000

    reverting

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-""" This module counts collected rain water """
+"""0-rain"""
 
 
 def rain(walls):
-    """
-        rain - function to perform the count
-        @walls - incoming list of walls to retain water
-        Return - returns the amount of rainwater, 0 if list doesn't exist
-    """
+    """rain function"""
+
+    water = 0
 
     if len(walls) == 0:
         return 0
-    if not walls:
-        return 0
+    left, right = 0, len(walls) - 1
+    leftMax, rightMax = walls[left], walls[right]
+    water = 0
 
-    raincount = 0
-
-    for i in range(0, len(walls)):
-        if walls[i] != 0:
-            for j in range(i + 1, len(walls)):
-                if walls[j] != 0:
-                    if walls[i] <= walls[j]:
-                        height = walls[i]
-                    else:
-                        height = walls[j]
-                    raincount += ((j - i) - 1) * height
-                    break
-
-    return raincount
+    while left < right:
+        if leftMax < rightMax:
+            left += 1
+            leftMax = max(leftMax, walls[left])
+            water += leftMax - walls[left]
+        else:
+            right -= 1
+            rightMax = max(rightMax, walls[right])
+            water += rightMax - walls[right]
+    return water
