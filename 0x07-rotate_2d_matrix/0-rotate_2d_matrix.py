be87a1767ae1db8806488ee63152d8dfbb6c2131
szbrooks2017 <strattonbrooks13@gmail.com>
2022-04-06 20:56:22 +0000

    removing import

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import math
 
 """
 rotate_2d_matrix - rotates a 2d matrix in place
@@ -11,23 +10,21 @@ def rotate_2d_matrix(matrix):
     "find the length of the matrix"
 
     # k, temp = 0, 0
-    # angle = -90
+    # angle = 90
     # row = len(matrix)
     # column = len(matrix[0])
     # rotate = [[math.cos(angle), math.sin(angle)],
     #            [-1 * math.sin(angle), math.cos(angle)]]
 
     # for i in range(column):
-    #     for j in range(row):
+    #     for j in range(i, column):
     #         temp = 0
     #         while k < 2:
     #             temp += round(matrix[i][k] * rotate[k][j])
     #             k = k + 1
     #     matrix[i][j] = temp
-    # print(matrix)
-    # return matrix
-    row = len(matrix)
 
+    row = len(matrix)
     # transpose
     for i in range(row):
         for j in range(i, row):
