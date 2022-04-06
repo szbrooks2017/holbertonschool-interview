891636a4818969c70e6078d1eab43ed4e08f087f
szbrooks2017 <strattonbrooks13@gmail.com>
2022-04-06 00:13:20 +0000

    switching the right direction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,13 +10,27 @@ Return: nothing
 def rotate_2d_matrix(matrix):
     "find the length of the matrix"
 
+    # k, temp = 0, 0
+    # angle = -90
+    # row = len(matrix)
+    # column = len(matrix[0])
+    # rotate = [[math.cos(angle), math.sin(angle)],
+    #            [-1 * math.sin(angle), math.cos(angle)]]
+
+    # for i in range(column):
+    #     for j in range(row):
+    #         temp = 0
+    #         while k < 2:
+    #             temp += round(matrix[i][k] * rotate[k][j])
+    #             k = k + 1
+    #     matrix[i][j] = temp
+    # print(matrix)
+    # return matrix
     row = len(matrix)
-    column = len(matrix[0])
-    # rotate = [[math.cos(90), math.sin(90)],
-    #           [-1 * math.sin(90), math.cos(90)]]
+
     # transpose
     for i in range(row):
-        for j in range(column):
+        for j in range(i, row):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     # reverse
@@ -24,4 +38,3 @@ def rotate_2d_matrix(matrix):
     for i in range(n//2):
         for j in range(n):
             matrix[j][i], matrix[j][n-1-i] = matrix[j][n-1-i], matrix[j][i]
-    return matrix
